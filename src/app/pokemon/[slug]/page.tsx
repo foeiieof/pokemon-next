3e4fdbb12ba57a780b240d81f8d5736b887d521c
foeiieof <akarawat.chant@gmail.com>
2025-02-19 00:45:55 +0700

    update : components on PokemonPage

--- src/app/pokemon/[slug]/page.tsx
+++ src/app/pokemon/[slug]/page.tsx
@@ -5,16 +5,19 @@ const PokemonPage = React.memo(({ params }: { params: { slug: string } }) => {
   const name = useMemo(() => decodeURIComponent(params.slug), [params.slug])
   console.log("Rendering PokemonId with name:", name);
   return (
-    <div className="bg-gray-300/30 grid grid-rows-[20px_1fr_20px] min-h-[100vh] items-center justify-items-center px-16 py-10 font-[family-name:var(--font-geist-sans)]">
-      <Suspense fallback={
-        <div className="w-full h-[100vh] bg-gray-300/30 flex justify-center items-center py-10">
-          <div className="w-[50vw] h-full bg-white/60 flex justify-center items-center rounded-md">
+    <main
+      className="w-full h-full border rounded-md  flex flex-wrap gap-8 row-start-2 justify-around items-center "
+    >
+      <div className="w-full h-[100vh] bg-gray-300/30 flex justify-center items-center py-10">
+        <Suspense fallback={
+          <div className="w-[70vw] sm:h-[80vh] bg-white/60 flex justify-center items-center rounded-md">
             <p className="font-bold text-[42px] w-full text-center"><span className="transition-transform ease-in-out duration-300 after:contents-['·...'] after:animate-movingDots"></span></p>
           </div>
-        </div>}>
-        <PokemonId name={name} />
-      </Suspense>
-    </div>
+        }>
+          <PokemonId name={name} />
+        </Suspense>
+      </div>
+    </main>
   )
 })
 PokemonPage.displayName = "PokemonPage"
